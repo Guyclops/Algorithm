ff3fa9d78a4e756ed0d0432fe59d0b0d35b6cc01
Guyclops <wnsxo0816@gmail.com>
2018-01-23 22:56:11 +0900

    20계단

---
+++ 더블릿/20계단/cantoring.java
@@ -0,0 +1,26 @@
+﻿import java.util.Scanner;
+
+public class Main{
+    static void cantoring(double n){
+        if(n==3){
+            System.out.print("- -");
+            return;
+        }
+        cantoring(n/3);
+        for(int i=0;i<n/3;i++){
+            System.out.print(" ");
+        }
+        cantoring(n/3);
+    }
+
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        if(n==0){
+            System.out.println("-");
+            return;
+        }
+        double i = Math.pow(3,n);
+        cantoring(i);
+    }
+}
---
+++ 더블릿/20계단/color.java
@@ -0,0 +1,43 @@
+﻿import java.util.Scanner;
+
+public class Main{
+    static int[][] arr;
+    static int pink = 0;
+    static int white = 0;
+    static void color(int n, int x1, int y1, int x2, int y2){
+        int w = 0;
+        int p = 0;
+
+        for(int i=x1;i<x2;i++){
+            for(int j=y1;j<y2;j++){
+                if(arr[i][j]==1)
+                   p++;
+                else
+                    w++;
+            }
+        }
+        if(w==(n*n)){
+            white++;
+        }else if(p==(n*n)){
+            pink++;
+        }else{
+            color(n/2,x1,y1,(x2+x1)/2,(y2+y1)/2);
+            color(n/2,x1,(y2+y1)/2,(x2+x1)/2,y2);
+            color(n/2,(x2+x1)/2,y1,x2,(y2+y1)/2);
+            color(n/2,(x2+x1)/2,(y2+y1)/2,x2,y2);
+        }
+    }
+    public static void main(String[] args){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        arr = new int[n][n];
+        for(int i=0;i<n;i++){
+            for(int j=0;j<n;j++){
+                arr[i][j] = in.nextInt();
+            }
+        }
+        color(n,0,0, n , n);
+        System.out.println(white);
+        System.out.println(pink);
+    }
+}
